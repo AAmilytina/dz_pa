01b6e0c8a8f97f53ad82e2d945bf1b1d4dc60373
AAmilytina <milytinaa@gmail.com>
2023-11-30 15:03:22 +0300

    Z1 done

---
+++ Z1/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int n = int.Parse(Console.ReadLine()!);
+n = Math.Abs(n);
+if (n%7==0 && n%23==0)
+    Console.WriteLine("да");
+else 
+    Console.WriteLine("нет");
