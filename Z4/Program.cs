876f95a786b31d7bacff2910a4ceba945ae114ce
AAmilytina <milytinaa@gmail.com>
2023-11-30 18:26:33 +0300

    Z4 done

---
+++ Z4/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+Console.Write("Введите натулаьное число: ");
+int n = int.Parse(Console.ReadLine()!);
+if (n<10)
+    Console.WriteLine($"n");
+else
+{
+    while (n>0)
+    {
+        int res = n%10;
+        n /=  10;
+        if (n>0)
+            Console.WriteLine(res + ",");
+        else 
+        Console.WriteLine(res);
+    }
+}
