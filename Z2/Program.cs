d003218995743c8da6c0696314ededc59871d004
AAmilytina <milytinaa@gmail.com>
2023-11-30 15:45:11 +0300

    Z2 done

---
+++ Z2/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.Write("Введите значение координаты точки x: ");
+int x = int.Parse(Console.ReadLine()!);
+Console.Write("Введите значение координаты точки y: ");
+int y = int.Parse(Console.ReadLine()!);
+if ((y>0) && (x>0))
+    Console.WriteLine("Точка в первой координатной  плоскости.");
+if ((y>0) && (x<0))
+    Console.WriteLine("Точка в второй координатной  плоскости."); 
+if ((y<0) && (x<0))
+    Console.WriteLine("Точка в третьей координатной  плоскости.");  
+if ((y<0) && (x>0))
+    Console.WriteLine("Точка в четвертой координатной  плоскости.");
