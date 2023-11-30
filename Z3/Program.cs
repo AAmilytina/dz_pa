9f17f15fc5c862bfcdbcefebb761175df7ec80e0
AAmilytina <milytinaa@gmail.com>
2023-11-30 17:35:47 +0300

    Z3 done

---
+++ Z3/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+Console.Write("Введите целое число от 10 до 99:  ");
+int n = int.Parse(Console.ReadLine()!);
+if (n<10 || n>99)
+   { Console.WriteLine("Вы ошиблись!\nВведите целое число от 10 до 99.");
+    n = int.Parse(Console.ReadLine()!);
+   }
+else{
+    int n1 = n/10;
+    int n2 = n%10;
+    if (n1>n2)
+    Console.WriteLine($"{n1}");
+    else
+    Console.WriteLine($"{n2}");
+}
